3d2b179175beaba47f4fed6fdc025fb7f193290d
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2022-11-23 12:35:15 +0900

    sub) 연구 정보 - 특허증 표시 수정
    이전/다음 버튼을 이용해 특허증을 하나씩 볼 수 있도록 함

---
+++ research_js/research.js
@@ -0,0 +1,92 @@
+//sub_2_0.js
+
+let num=0
+// btnEl.addEventListener('click',translat);
+
+// function translat() {
+//     paperEl.style.display='block'
+//     paperEl.style = {
+//         'height' : '500px'
+//     }
+// }
+if (screen.width < 768) {
+  $('#tech .pop').on('click', function(){
+    $('#paper').css({
+      display: 'block'
+    })
+    $('#paper .images').css({
+      left: '0px'
+    })
+    $('#paper .images img').css({
+      opacity : '0'
+     })
+     $('#paper .images img').eq(0).css({
+      opacity: "1"
+     })
+  })
+  $('#paper .close').on('click', function(){
+    $('#paper').css({
+      display : 'none'
+    })
+    num=0
+    console.log(num)
+  })
+  $('#paper .images').on('click',function(){
+    num++
+     $('#paper .images img').css({
+      opacity : '0'
+     })
+     $('#paper .images img').eq(num%14).css({
+      opacity: "1"
+     })
+  })
+} else if (screen.width > 768) {
+  $('#tech .pop').on('click', function(){
+    $('#paper').css({
+      display: 'block'
+    })
+    $('#paper .images').css({
+      position: 'absolute',
+      left: '0px'
+    })
+    console.log($('#paper .img_box .images').offset())
+  })
+  $('#paper .close').on('click', function(){
+    $('#paper').css({
+      display : 'none'
+    })
+    num=0;
+  })
+  $('#paper .images').css({
+    position: 'absolute',
+    left : '0'
+  })
+  $('#paper .direc .left').on('click',function(){
+    console.log($('#paper .img_box .images').position().left)
+    if($('.images').position().left>-10) {
+      alert('처음입니다.')
+    }else {
+      num++
+      let numb = 485*num
+      $('.images').css({
+        position : 'absolute',
+        left : `${numb}px`
+      })
+    }
+  })
+  $('#paper .direc .right').on('click',function(){
+    console.log($('#paper .img_box .images').position().left)
+    if($('.images').position().left<-4850) {
+      alert('마지막입니다.')
+    }else {
+      num--
+      let numb = 485*num
+      $('.images').css({
+        position : 'absolute',
+        left : `${numb}px`
+      })
+    }
+  })
+
+
+}
